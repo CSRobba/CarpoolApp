7ff0f0052ea49fceef12bb3df51f7ab84c2321b9
Chandana Robba <chandanarobba@chandanaslaptop.lan>
2025-07-17 16:07:03 -0700

    Add root endpoint and confirm backend-frontend connection

--- backend/CarpoolApp_backend/src/main/java/com/example/carpoolapp_backend/CarpoolAppBackendApplication.java
+++ backend/CarpoolApp_backend/src/main/java/com/example/carpoolapp_backend/CarpoolAppBackendApplication.java
@@ -2,6 +2,8 @@ package com.example.carpoolapp_backend;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 public class CarpoolAppBackendApplication {
@@ -10,4 +12,17 @@ public class CarpoolAppBackendApplication {
         SpringApplication.run(CarpoolAppBackendApplication.class, args);
     }
 
+}
+
+@RestController
+class DemoController {
+    @GetMapping("/")  // Handling the root endpoint
+    public String home() {
+        return "Carpool Backend is running! Go to /test for the test endpoint.";
+    }
+
+    @GetMapping("/test")
+    public String test() {
+        return "Backend connected!";
+    }
 }
